07d53881f1ef3996f703b4a112fd717eedf3c08c
therobocademy <therobocademy@gmail.com>
2026-05-15 17:42:26 +0530

    Updated lift demo

--- src/isaac_so_arm101/tasks/lift/mdp/observations.py
+++ src/isaac_so_arm101/tasks/lift/mdp/observations.py
@@ -15,6 +15,7 @@ from typing import TYPE_CHECKING
 import torch
 from isaaclab.assets import RigidObject
 from isaaclab.managers import SceneEntityCfg
+from isaaclab.sensors import FrameTransformer
 from isaaclab.utils.math import subtract_frame_transforms
 
 if TYPE_CHECKING:
@@ -34,3 +35,28 @@ def object_position_in_robot_root_frame(
         robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], object_pos_w
     )
     return object_pos_b
+
+
+def ee_position_in_robot_root_frame(
+    env: ManagerBasedRLEnv,
+    robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+    ee_frame_cfg: SceneEntityCfg = SceneEntityCfg("ee_frame"),
+) -> torch.Tensor:
+    """The end-effector position in the robot's root frame."""
+    robot: RigidObject = env.scene[robot_cfg.name]
+    ee_frame: FrameTransformer = env.scene[ee_frame_cfg.name]
+    ee_pos_w = ee_frame.data.target_pos_w[..., 0, :]
+    ee_pos_b, _ = subtract_frame_transforms(robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], ee_pos_w)
+    return ee_pos_b
+
+
+def object_to_ee_position_in_robot_root_frame(
+    env: ManagerBasedRLEnv,
+    robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+    object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+    ee_frame_cfg: SceneEntityCfg = SceneEntityCfg("ee_frame"),
+) -> torch.Tensor:
+    """Vector from the end-effector to the object, expressed in the robot's root frame."""
+    return object_position_in_robot_root_frame(env, robot_cfg, object_cfg) - ee_position_in_robot_root_frame(
+        env, robot_cfg, ee_frame_cfg
+    )
